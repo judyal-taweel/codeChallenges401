0e8f9fdd8cbac7a389a88251a8593e5179c08a8a
judyal-taweel <judybataineh@gmail.com>
2021-08-11 20:09:10 +0300

    challenge 28

--- challenges/app/src/test/java/challenges/AppTest.java
+++ challenges/app/src/test/java/challenges/AppTest.java
@@ -4,8 +4,12 @@
 package challenges;
 
 import Insertion.MargeSort;
+import Insertion.QuickSort;
 import Insertion.Sort;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
@@ -26,5 +30,23 @@ class AppTest {
     public void testMergeSort() {
         MargeSort mergeSort= new MargeSort();
         assertArrayEquals(new Integer[]{5, 10, 15, 25, 30}, mergeSort.mergeSort(integerArr));
+    }
+
+    @Test void quickSortTest(){
+
+        int arr[] = {8,4,23,42,16,15};
+        QuickSort quickSort=new QuickSort();
+        quickSort.quickSort(arr,0,arr.length-1);
+
+        String res= Arrays.toString(arr);
+        assertEquals("[4, 8, 15, 16, 23, 42]",res); }
+
+    @Test void newQuickTest(){
+
+        int arr[] = {8,4,23,1,42,16,15};
+        QuickSort quickSort=new QuickSort();
+        quickSort.quickSort(arr,0,arr.length-1);
+
+        String res= Arrays.toString(arr); assertEquals("[1, 4, 8, 15, 16, 23, 42]",res);
     }
 }
